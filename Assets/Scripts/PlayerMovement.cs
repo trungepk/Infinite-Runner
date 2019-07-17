e18a84678c7082bb81e28951a60f8b600d4ed732
HOPNX\hopnx <hop.nguyen@gihot.vn>
2019-07-17 18:04:58 +0700

    Player movement and scrolling path

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float sidewaySpeed = 1000f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //if (!GameControl.instance.isGameOver)
+        //{
+            var sideMovement = Input.GetAxis("Horizontal") * sidewaySpeed;
+            rb.AddForce(sideMovement * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        //}
+    }
+}
---
+++ Assets/Scripts/ScrollingPath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingPath : MonoBehaviour {
+
+    [SerializeField] private float scrollingSpeed = 5f;
+
+    private BoxCollider pathCollider;
+    private float pathLength;
+
+	void Start () {
+        //pathCollider = GetComponent<BoxCollider>();
+        pathLength = transform.localScale.z;
+	}
+	
+	void Update () {
+        transform.position -= new Vector3(0, 0, 1) * Time.deltaTime * scrollingSpeed;
+        if(transform.position.z < -pathLength)
+        {
+            Repositioning();
+        }
+	}
+
+    private void Repositioning()
+    {
+        Vector3 pathOffset = new Vector3(0, 0, pathLength * 2);
+        transform.position += pathOffset;
+    }
+}
