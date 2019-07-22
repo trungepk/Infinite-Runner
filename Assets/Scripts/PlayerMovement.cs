17b6a2912c7f123a880f86e62c082b5fd3c70d66
HOPNX\hopnx <hop.nguyen@gihot.vn>
2019-07-22 14:59:40 +0700

    Fix clunky movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,17 @@ public class PlayerMovement : MonoBehaviour {
     private bool canMove = true;
 
     private Animator anim;
-    [System.NonSerialized] public float idleTime;
-    [System.NonSerialized] public float shrinkingTime;
-    [System.NonSerialized] public float swellingTime;
+    //[System.NonSerialized] public float idleTime;
+    //[System.NonSerialized] public float shrinkingTime;
+    //[System.NonSerialized] public float swellingTime;
 
     private float lerpTime = 1f;
     private float currentLerpTime;
+    private Vector3 shrinkAndSwellScale;
+    private Vector3 duckScale;
+    private Vector3 originalScale;
 
+    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float jumpVelocity = 5f;
     [SerializeField] private float fallMultiplier = 2.5f;
@@ -24,34 +28,34 @@ public class PlayerMovement : MonoBehaviour {
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-        UpdateAnimClipLength();
-
+        //UpdateAnimClipLength();
+        originalScale = transform.localScale;
+        shrinkAndSwellScale = originalScale;
     }
 
-    private void UpdateAnimClipLength()
-    {
-        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+    //private void UpdateAnimClipLength()
+    //{
+    //    AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
 
-        foreach (AnimationClip clip in clips)
-        {
-            switch (clip.name)
-            {
-                case "Idle":
-                    idleTime = clip.length;
-                    break;
-                case "Shrinking":
-                    shrinkingTime = clip.length;
-                    break;
-                case "Swelling":
-                    swellingTime = clip.length;
-                    break;
-            }
-        }
-    }
+    //    foreach (AnimationClip clip in clips)
+    //    {
+    //        switch (clip.name)
+    //        {
+    //            case "Idle":
+    //                idleTime = clip.length;
+    //                break;
+    //            case "Shrinking":
+    //                shrinkingTime = clip.length;
+    //                break;
+    //            case "Swelling":
+    //                swellingTime = clip.length;
+    //                break;
+    //        }
+    //    }
+    //}
     private void FixedUpdate()
     {
         Jump();
-        Debug.Log(rb.velocity);
     }
 
     private void Jump()
@@ -74,6 +78,7 @@ public class PlayerMovement : MonoBehaviour {
     void Update()
     {
         MoveSideWay();
+        Duck();
     }
 
     private void MoveSideWay()
@@ -96,27 +101,102 @@ public class PlayerMovement : MonoBehaviour {
         transform.position = new Vector3((float)Math.Round(transform.position.x), transform.position.y, transform.position.z);
     }
 
+    //private IEnumerator GoLeft(float perc)
+    //{
+    //    canMove = false;
+    //    anim.SetTrigger(Constants.ShrinkAnim);
+    //    yield return new WaitForSeconds(shrinkingTime + 0.1f);
+    //    Vector3 left = transform.position + Vector3.left;
+    //    transform.position = Vector3.Lerp(transform.position, left, perc);
+    //    anim.SetTrigger(Constants.SwellAnim);
+    //    yield return new WaitForSeconds(swellingTime);
+    //    canMove = true;
+    //}
+
+    //private IEnumerator GoRight(float perc)
+    //{
+    //    canMove = false;
+    //    anim.SetTrigger(Constants.ShrinkAnim);
+    //    yield return new WaitForSeconds(shrinkingTime + 0.1f);
+    //    Vector3 right = transform.position + Vector3.right;
+    //    transform.position = Vector3.Lerp(transform.position, right, perc);
+    //    anim.SetTrigger(Constants.SwellAnim);
+    //    yield return new WaitForSeconds(swellingTime);
+    //    canMove = true;
+    //}
+
+    
     private IEnumerator GoLeft(float perc)
     {
         canMove = false;
-        anim.SetTrigger(Constants.ShrinkAnim);
-        yield return new WaitForSeconds(shrinkingTime + 0.1f);
+        while (true)
+        {
+            shrinkAndSwellScale = transform.localScale;
+            shrinkAndSwellScale.x -= Time.deltaTime * moveSpeed;
+            shrinkAndSwellScale.z -= Time.deltaTime * moveSpeed;
+            transform.localScale = shrinkAndSwellScale;
+            yield return new WaitForEndOfFrame();
+            if(shrinkAndSwellScale.x <= 0 ) { break; }
+        }
+        
         Vector3 left = transform.position + Vector3.left;
         transform.position = Vector3.Lerp(transform.position, left, perc);
-        anim.SetTrigger(Constants.SwellAnim);
-        yield return new WaitForSeconds(swellingTime);
+
+        while (true)
+        {
+            shrinkAndSwellScale = transform.localScale;
+            shrinkAndSwellScale.x += Time.deltaTime * moveSpeed;
+            shrinkAndSwellScale.z += Time.deltaTime * moveSpeed;
+            transform.localScale = shrinkAndSwellScale;
+            yield return new WaitForEndOfFrame();
+            if (shrinkAndSwellScale.x >= originalScale.x) { break; }
+        }
+
+        transform.localScale = originalScale;
         canMove = true;
     }
-
     private IEnumerator GoRight(float perc)
     {
         canMove = false;
-        anim.SetTrigger(Constants.ShrinkAnim);
-        yield return new WaitForSeconds(shrinkingTime + 0.1f);
+        while (true)
+        {
+            shrinkAndSwellScale = transform.localScale;
+            shrinkAndSwellScale.x -= Time.deltaTime * moveSpeed;
+            shrinkAndSwellScale.z -= Time.deltaTime * moveSpeed;
+            transform.localScale = shrinkAndSwellScale;
+            yield return new WaitForEndOfFrame();
+            if (shrinkAndSwellScale.x <= 0) { break; }
+        }
+
         Vector3 right = transform.position + Vector3.right;
         transform.position = Vector3.Lerp(transform.position, right, perc);
-        anim.SetTrigger(Constants.SwellAnim);
-        yield return new WaitForSeconds(swellingTime);
+
+        while (true)
+        {
+            shrinkAndSwellScale = transform.localScale;
+            shrinkAndSwellScale.x += Time.deltaTime * moveSpeed;
+            shrinkAndSwellScale.z += Time.deltaTime * moveSpeed;
+            transform.localScale = shrinkAndSwellScale;
+            yield return new WaitForEndOfFrame();
+            if (shrinkAndSwellScale.x >= originalScale.x) { break; }
+        }
+
+        transform.localScale = originalScale;
         canMove = true;
+    }
+
+    
+    private void Duck()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            duckScale = transform.localScale;
+            duckScale.y /= 2f;
+            transform.localScale = duckScale;
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            transform.localScale = originalScale;
+        }
     }
 }
